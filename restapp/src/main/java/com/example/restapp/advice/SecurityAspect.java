cfabf58c8e36e36818273224b69080a2db977ae2
= <=>
2022-01-02 19:21:04 +0900

    Update SecurityAspect.java

--- restapp/src/main/java/com/example/restapp/advice/SecurityAspect.java
+++ restapp/src/main/java/com/example/restapp/advice/SecurityAspect.java
@@ -18,7 +18,7 @@ import javax.xml.bind.DatatypeConverter;
 @Component
 public class SecurityAspect {
 
-    @Before("@annotation(tokenRequired)")
+    @Before("@annotation(tokenRequired)")       // 해당 annotation이 적용된 메소드는 아래 코드가 수행된다.
     public void authWithToken(TokenRequired tokenRequired) {
         ServletRequestAttributes reqAttributes =
                 (ServletRequestAttributes)
@@ -26,14 +26,14 @@ public class SecurityAspect {
         HttpServletRequest request = reqAttributes.getRequest();
         // checks for token in request header
         String tokenInHeader = request.getHeader("token");
-        if(StringUtils.isEmpty(tokenInHeader)){
+        if(StringUtils.isEmpty(tokenInHeader)){     // 요청 헤드 확인 -> token이라는 이름으로 왔는지
             throw new IllegalArgumentException("Empty token");
         }
-        Claims claims = Jwts.parser().setSigningKey(
+        Claims claims = Jwts.parser().setSigningKey(        // 위의 token의 경우 수행
                         DatatypeConverter.parseBase64Binary(
                                 SecurityServiceImpl.secretKey))
                 .parseClaimsJws(tokenInHeader).getBody();
-        if(claims == null || claims.getSubject() == null) {
+        if(claims == null || claims.getSubject() == null) {     // 그렇지 않은 경우 예외 발생
             throw new IllegalArgumentException("Token Error : Claim is null");
         }
         System.out.println("토큰에 포함된 subject로 자체 인증처리 필요..");
